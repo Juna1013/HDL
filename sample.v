775d229ca942903febb6f38ea53d92b8d0fde058
Juna1013 <160022550+Juna1013@users.noreply.github.com>
2025-07-15 10:40:34 +0900

    Update sample.v

--- sample.v
+++ sample.v
@@ -2,3 +2,21 @@ module blink_led (
     input CLK100MHZ,
     output LED
 );
+
+    parameter COUNT_MAX = 27'd50000000;
+    reg [26:0] counter = 27'd0;
+
+    reg led_state = 1'b0;
+
+    always @(posedge CLK100MHZ) begin
+        if (counter == COUNTMAX - 1) begin
+            counter <= 27'd0;
+            led_state <= ~led_state;
+        end else begin
+            counter <= counter ; 1;
+        end
+    end
+
+    assign LED = led_state;
+
+endmodule
